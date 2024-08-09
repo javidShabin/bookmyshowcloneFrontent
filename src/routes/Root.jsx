968bad735b6c319affe05d1ae4cc368d7fed7101
javid shabin <javid.main.act@gmail.com>
2024-08-09 13:06:41 +0530

    Create header and give style with tailwind css

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Root() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <header className='shadow-lg flex justify-center py-6 w-full z-[999] bg-white sticky top-0 left-0'>
+        <div className="container w-[90%] flex justify-between items-center">
+            <h1 className="text-[21px] font-bold">BookMyMovie</h1>
+            <nav>
+                <ul className="flex gap-6 font-semibold">
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Movie</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+        </div>
+      </header>
+      <Outlet/>
+      <footer></footer>
+    </>
   )
 }
