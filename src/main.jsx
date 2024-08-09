d77914f33869a27a86cba0881367191b6dae8a15
javid shabin <javid.main.act@gmail.com>
2024-08-09 12:59:36 +0530

    Create react app with vite and remove unwanted files and install tail wind and react router. Then config both libreries for the project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Root from './routes/Root';
+import ErrorPage from './Error-page';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root/>,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
