8bc43421dcd09b5b67475c8fd5495de97305891e
javid shabin <javid.main.act@gmail.com>
2024-08-09 19:55:15 +0530

    Make responsiveness the movi section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,30 @@
-import React, {useEffect, useState} from 'react'
-import MovieCard from './MovieCard'
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
 export default function Home() {
-    // Get movie form the api
-    const [movies, setMovies] = useState([])
-    const getAllMovie = async () => {
-        const response = await fetch('http://localhost:4000/movies')
-        const movies = await response.json()
-        setMovies(movies)
-    }
+  // Get movie form the api
+  const [movies, setMovies] = useState([]);
+  const getAllMovie = async () => {
+    const response = await fetch("http://localhost:4000/movies");
+    const movies = await response.json();
+    setMovies(movies);
+  };
 
-    // Use Effect hook
-    useEffect(()=>{
-        // Get movie function
-        getAllMovie()
-    },[])
-    
+  // Use Effect hook
+  useEffect(() => {
+    // Get movie function
+    getAllMovie();
+  }, []);
+
   return (
     <main>
-        <section>
-            {movies.map((movie) => {
-                return <MovieCard key={movie._id} movie={movie}/>
-            })}
-        </section>
+      <section className="w-[99%] mx-auto">
+        <div className="mx-auto grid grid-cols-1 gap-5 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {movies.map((movie) => {
+            return <MovieCard key={movie._id} movie={movie} />;
+          })}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
--- src/routes/MovieCard.jsx
+++ src/routes/MovieCard.jsx
@@ -5,9 +5,9 @@ export default function MovieCard(props) {
   const { movie } = props;
   return (
     <main>
-      <div>
-        <img src={movie.thumbnail} />
-        <h2>{movie.title}</h2>
+      <div className="text-center">
+        <img src={movie.thumbnail} className="min-w-20 rounded-md shadow-2xl " />
+        <h2 className="font-semibold mt-3 text-[18px]">{movie.title}</h2>
       </div>
     </main>
   );
